f89f143025559d05000522992c7739de203bcae0
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-12-26 20:38:59 +0100

    Finished

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default async function Home() {
   return (
-    <section className='not-found'>
+    <section>
       <h1>Página de inicio</h1>
       <hr />
       <Link href={"/articulos"}>Listado de artículos</Link>      
