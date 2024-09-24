2c4ebb742fb347e0271d5bdcda31449802b82c30
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2024-09-24 13:53:36 +0200

    string mysql url

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -1,11 +1,15 @@
 import mysql from 'serverless-mysql'
 
-export const db = mysql({
-    config: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        port: 3306,
-        database: 'datos'
-    }
-})
+// Podemos inicializar la conexión de esta manera
+// export const db = mysql({
+//     config: {
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'root',
+//         port: 3306,
+//         database: 'crud'
+//     }
+// })
+
+// También podemos inicializar la conexión de esta otra manera
+export const db = mysql('mysql://root@localhost:3306/crud')
